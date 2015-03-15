2d67d591557f9f1f3e3d05436b5bb2e7f7727f61
PaulusWeb <pavel_tregubov@mail.ru>
2015-03-15 12:25:43 +0300

    Revert "for code review"
    This reverts commit c480b2611f2c0b92ae97b32c3536dd9199bcf384.

--- src/engine/math/mathlib.h
+++ src/engine/math/mathlib.h
@@ -1,12 +1,4 @@
 #ifndef MATHLIB_H
-#define MATHLIB_H
+    #define MATHLIB_H
 
-namespace Math
-{
-    template <typename T> inline
-    int sign(T x)
-    {
-        return x == 0 ? 0 : x / abs(x);
-    }
-}
 #endif
