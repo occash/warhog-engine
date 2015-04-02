d8e4f715548d57113abe5020e626564940f922c7
PaulusWeb <pavel_tregubov@mail.ru>
2015-04-02 11:32:11 +0300

    Delete soundlog

--- src/engine/soundlog.cpp
+++
@@ -1,7 +0,0 @@
-#include "soundlog.h"
-#include <stdio.h>
-void err_check(FMOD_RESULT result, char *file, int line)
-{
-    if (result != FMOD_OK)
-        printf("FMOD error! file:%s line:%d  %s\n", file, line, FMOD_ErrorString(result));
-}
--- src/engine/soundlog.h
+++
@@ -1,7 +0,0 @@
-#ifndef SOUNDLOG_H
-    #define SOUNDLOG_H
-    #include <fmod/fmod_errors.h>
-    #include <windows.h>
-    void err_check(FMOD_RESULT result, char *file, int line);
-    #define ERRCHECK(_result) err_check(_result, __FILE__, __LINE__)
-#endif
